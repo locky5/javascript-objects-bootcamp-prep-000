b1b4ffc70b02d2b4db0b31d674275049177e6d5b
Kevin Dai <daikevin19@yahoo.com>
2019-04-04 21:40:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({ keys: `0` })
 
-function updatePlayist(playlist,artist,song) {
-  playlist[artist]=song
+function updatePlayist(playlist,artistName,songTitle) {
+  playlist[artistName]=songTitle
   return playlist
 }
